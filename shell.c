57b338141810e0bfe7afe2205ce5716d97af111b
karama Gueye <124457622+karamagueye@users.noreply.github.com>
2023-11-03 01:48:24 +0000

    simple_shell

---
+++ shell.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * Description: Display a prompt and wait for the user to type a command.
+ * A command line always ends with a new line.
+ * Return: nathing
+ */
+
+int main()
+{
+        char input[100];
+        char prompt[] = "simple_shell";
+
+        while (1)
+        {
+        printf("simple_shell> ");
+        char command[100];
+        if (fgets(command, sizeof(command), stdin) == NULL)
+        {
+                printf("\n");
+                break;
+        }
+        size_t len = strlen(command);
+        if (len > 0 && command[len - 1] == '\n')
+        {
+                command[len - 1] = '\0';
+        }
+        if (system(command) == -1)
+        {
+
+                printf("Erreur : commande not found\n");
+        }
+
+        return 0;
+        }
+}
